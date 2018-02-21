ddc157bbf04abd9e43070531c4c0dc3feaf6beeb
ivzhang1 <izhang1@stuy.edu>
2018-02-21 12:04:51 -0500

    minor edits

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -2,18 +2,17 @@ import java.util.ArrayList;
 
 public class KnightBoard{
     private int[][] board;
-    public int[][] heuristic;
     private int numRows;
     private int numCols;
-    private int[][] moves = {{1, -2}, {1, 2},
+    private int[][] moves = {{2, 1}, {1, 2},
 			   {-1,-2}, {-1, 2},
 			   {2, -1}, {-2, -1},
-			   {2,1}, {-2, 1}};
+			   {1,-2}, {-2, 1}};
 
     public static void main(String[] args){
-	KnightBoard n = new KnightBoard(7,7);
-	//System.out.println(n.solve(0,0));
-	//System.out.println(n);
+	KnightBoard n = new KnightBoard(5,5);
+	System.out.println(n.solve(0,0));
+	System.out.println(n);
 	//n = new KnightBoard(5,5);
 	System.out.println(n.countSolutions(0,0));
 	System.out.println(n);
@@ -39,8 +38,6 @@ public class KnightBoard{
 	board = new int[startingRows][startingCols];
 	numRows = startingRows;
 	numCols = startingCols;
-	heuristic = new int[startingRows][startingCols];
-	heuristic();
     }
 	
     //see format for toString below
@@ -64,7 +61,7 @@ public class KnightBoard{
 
     private boolean hasNonZero(){
 	for (int r = 0; r < board.length; r++){
-	    for (int c = 0; c < board.length; c++){
+	    for (int c = 0; c < board[r].length; c++){
 		if (board[r][c] != 0){
 		    return true;
 		}
@@ -123,7 +120,7 @@ public class KnightBoard{
     
     public int countSolutions(int startingRow, int startingCol){
 	checkConditions(startingRow, startingCol);
-	return countHeuristic(startingRow, startingCol, 1);
+	return countH(startingRow, startingCol, 1);
     }
 
     private int countH(int r, int c, int level){
@@ -149,63 +146,4 @@ public class KnightBoard{
 	return total;
     }
 
-    private int countHeuristic(int r, int c, int level){
-	int total = 0;
-	if (board[r][c] != 0){
-	    return 0;
-	}
-	if (numRows*numCols == level){
-	    board[r][c] = level;
-	    return 1;
-	}
-
-	for (int[] x: findShortestPath(r,c)){
-	    int nextRow = r + x[1];
-	    int nextCol = c + x[0];
-	    if (nextRow < 0 || nextCol < 0 || nextCol >= numCols || nextRow >= numRows){
-	    }
-	    else{
-		board[r][c] = level;
-		total += countH(nextRow, nextCol, level+1);
-		board[r][c] = 0;
-	    }
-	}
-	return total;
-    }
-
-    //Finds shortest next path and returns it [row,col]
-    public ArrayList<int[]> findShortestPath(int r, int c){
-	ArrayList<int[]> min = new ArrayList<int[]>();
-	int currentMin = 10;
-	
-	for(int[] set: moves){
-	    int nextRow = r + set[0];
-	    int nextCol = c + set[1];
-	    if (!(nextRow < 0 || nextCol < 0 || nextCol >= numCols || nextRow >= numRows || board[r][c] != 0)){
-		if (heuristic[nextRow][nextCol] <= currentMin){
-		    currentMin = heuristic[nextRow][nextCol];
-		    min.add(set);
-		}
-	    }
-	}
-	//System.out.println(min);
-	return min;
-    }
-
-    //Creates heurisitic
-    private void heuristic(){	
-	for(int r = 0; r < numRows; r++){
-	    for (int c = 0; c < numCols; c++){
-		int countMoves = 0;
-		for(int[] set: moves){
-		    int nextRow = r + set[0];
-		    int nextCol = c + set[1];
-		    if (!(nextRow < 0 || nextCol < 0 || nextCol >= numCols || nextRow >= numRows)){
-			countMoves++;
-		    }
-		}
-		heuristic[r][c] = countMoves;
-	    }
-	}
-    }
 }
