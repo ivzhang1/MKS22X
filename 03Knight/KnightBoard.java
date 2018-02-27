c2fd765162e0adae8f6bbeb201a0720ae46f3d9e
ivzhang1 <izhang1@stuy.edu>
2018-02-26 22:49:54 -0500

    Fixed exceptions (thanks Crystal

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -9,31 +9,112 @@ public class KnightBoard{
 			   {2, -1}, {-2, -1},
 			   {1,-2}, {-2, 1}};
 
+
     public static void main(String[] args){
-	KnightBoard n = new KnightBoard(5,5);
-	System.out.println(n.solve(0,0));
-	System.out.println(n);
-	//n = new KnightBoard(5,5);
-	System.out.println(n.countSolutions(0,0));
-	System.out.println(n);
-	// for (int r = 0; r < n.heuristic.length; r++){
-	//     for (int c = 0; c < n.heuristic[r].length; c++){
-	//  	System.out.print(n.heuristic[r][c]);
-	//     }
-	//     System.out.println();
-	// }
-	// int[] shortest = n.findShortestNext(3,1);
-	// for (int i: shortest){
-	//     System.out.println(i);
-	// }
-		
-	
-	
-    }
+	KnightBoard a = new KnightBoard(3,3);
+
+	System.out.println(a);
+	/* Prints
+	   _ _ _
+	   _ _ _
+	   _ _ _
+	*/
+
+	for (int i = 0; i < 3; i++){
+	    for (int j = 0; j < 3; j++){
+		if (a.solve(i,j)){
+		    System.out.println("There is an error with your solve method");
+		}
+	    }
+	} //prints nothing
+
+	System.out.println(a.countSolutions(0,0)); //prints 0
+
+
+
+	KnightBoard b = new KnightBoard(5,5);
+	System.out.println(b.solve(0,0)); //prints true
+	System.out.println(b); //prints a valid solution
+
+	try{
+	    b.solve(0,0);
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+	try{
+	    b.countSolutions(0,0);
+	}catch(IllegalStateException e){
+	    System.out.println("Error: The board contains non-zero values");
+	} //prints "Error: The board contains non-zero values"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(-1,0);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters in the constructor");
+	} //prints "Error: There cannot be negative parameters in the constructor"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(1,-1);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters in the constructor");
+	} //prints "Error: There cannot be negative parameters in the constructor"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(-1,-1);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters in the constructor");
+	} //prints "Error: There cannot be negative parameters in the constructor"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(5,5);
+	    b1.solve(0,-1);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters");
+	} //prints "Error: There cannot be negative parameters"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(5,5);
+	    b1.solve(-1,0);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters");
+	} //prints "Error: There cannot be negative parameters"
+
+	try{
+	    KnightBoard b1 = new KnightBoard(5,5);
+	    b1.solve(-1,-1);
+	}catch(IllegalArgumentException e){
+	    System.out.println("Error: There cannot be negative parameters");
+	} //prints "Error: There cannot be negative parameters"
+
+
+
+	for (int i = 0; i < 5; i++){
+	    for (int j = 0; j < 5; j++){
+		KnightBoard abc = new KnightBoard(5,5);
+		System.out.println(abc.solve(i,j)); //prints alternating lines of true/false starting with true
+	    }
+	}
+	KnightBoard c = new KnightBoard(5,5);
+
+	int totalSol = 0;
+	for (int i = 0; i < 5; i++){
+	    for (int j = 0; j < 5; j++){
+		totalSol += c.countSolutions(i,j);
+	    }
+	}
+
+	System.out.println(totalSol); //prints 1728
+
+	KnightBoard d = new KnightBoard(5,5);
+	System.out.println(d.countSolutions(0,0)); //prints 304
+
+    }	
+      
     
     public KnightBoard(int startingRows,int startingCols){
 	if (startingRows < 0 || startingCols < 0){
-	    throw new IllegalStateException();
+	    throw new IllegalArgumentException();
 	}
 	board = new int[startingRows][startingCols];
 	numRows = startingRows;
@@ -50,7 +131,12 @@ public class KnightBoard{
 	    for (int c = 0; c < numCols; c++){
 		String current = "" + board[r][c];
 		if (board[r][c] < 10){
-		     current = "_" + current;
+		    if (board[r][c] == 0){
+			current = "_";
+		    }
+		    else{
+			current = "_" + current;
+		    }
 		}
 		returned += "" + current + " ";
 	    }
