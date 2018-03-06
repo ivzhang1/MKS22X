7a6f27686609f22cbcbbd98ab53c91bb052e0953
ivzhang1 <izhang1@stuy.edu>
2018-03-05 20:03:30 -0500

    Started USACO

---
+++ 05USACO/USACO.java
@@ -0,0 +1,13 @@
+public class USACO{
+
+    public static void main(String[] args){
+	System.out.println(USACO.bronze(""));
+	System.out.println(USACO.silver(""));
+    }
+    
+    public static int bronze(String filename){
+    }
+
+    public static int silver(String filename){
+    }
+}
