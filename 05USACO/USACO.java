2a16253a7b3dc44742ed687e16f8afc3bcdda397
ivzhang1 <izhang1@stuy.edu>
2018-03-08 14:41:59 -0500

    automated commit and push

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -4,26 +4,44 @@ import java.io.*;
 public class USACO{
 
     public static void main(String[] args){
-	System.out.println(USACO.bronze(""));
-	System.out.println(USACO.silver(""));
+	//System.out.println(USACO.bronze(""));
+	System.out.println(USACO.silver("makelake/makelake.1.in"));
     }
     
     public static int bronze(String filename){
+	int row = 0;
+	int col = 0;
+	int elevation;
+	int instructions;
 	try{
-
-	}catch(
-	
+	    File f = new File(filename);
+	    Scanner s = new Scanner(f);
+	}catch(FileNotFoundException e){
+	    System.out.println("File Not Found");
+	    System.exit(1);
+	}
+	return 0;
     }
 
-    public static int silver(String filename) throws FileNotFoundException{
-   
+    public static int silver(String filename){
+	char[][] board;
+
+	try{
+	    File f = new File(filename);
+	    Scanner s = new Scanner(f);
+	    String firstL = s.nextLine();
+	    String[] lineOne = firstL.split(" ");
+	    board = new char[Integer.parseInt(lineOne[0])][Integer.parseInt(lineOne[1])];
+	    //System.out.println(board.length + " " + board[0].length);
+ 	    
+	}catch(FileNotFoundException e){
+	    System.out.println("File Not Found");
+	    System.exit(1);
+	}
+
+	return 0;
     }
 
-    public static int[][] parse(String filename)throws FileNotFoundException{
-	File n = new File(filename);
-	Scanner s = new Scanner(n);
-	
-    }
 
 
 }
