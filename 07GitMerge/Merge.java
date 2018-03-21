91e8c7cb4adb9b973360177d9441d22f09b55eab
ivzhang1 <izhang1@stuy.edu>
2018-03-21 15:02:09 -0400

    Work on this later

--- 07GitMerge/Merge.java
+++ 07GitMerge/Merge.java
@@ -1,5 +1,7 @@
 public class Merge{
-    public static void main(String[] args){}
+    public static void main(String[] args){
+	
+    }
 
     public static void mergesort(int[] data){
 	int[] temp = new int[data.length];
@@ -9,17 +11,54 @@ public class Merge{
 
     public static void msort(int[] data, int[] temp, int lo, int hi){
 	if (lo < hi){
-	    mid = (hi + lo)/2;
+	    int mid = (hi + lo)/2;
+
+	    for (int i = 0; i < temp.length; i++){
+		
+	    }
 	    msort(temp, data, lo, mid);
-	    msort(temp, data, mid + 1, hi);
+	    msort(temp, data, mid+1, hi);
 	    merge(data, temp, lo, mid, hi);
 	}
     }
 
     public static void merge(int[] data, int[] temp, int lo, int mid, int hi){
+
 	
+	while (lo <= hi){
+	    toString(data);
+	    if (temp[lo] < temp[hi]){
+		data[curr] = temp[lo];
+		lo++;
+		
+	    }
+
+	    else{
+		data[curr] = temp[hi];
+		hi-=1;
+	    }
+	    curr++;
+	}
+	
+
 	
     }
 
+    
+    private static void toString(int[] parti){
+	for (int k = 0; k < parti.length; k++){
+	    System.out.print(parti[k] + ", ");
+	}
+	System.out.println();
+    }
+
+    private static void toString(int[] parti, int start, int end){
+	System.out.print("[");
+	for (int k = start; k < end; k++){
+	    System.out.print(parti[k] + ", ");
+	}
+	System.out.print("]");
+	System.out.println();
+    }
     
 }
---
+++ 07GitMerge/MergeDriverTwo.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MergeDriverTwo{
+
+    public static void main(String[]args){
+	//int[] ary = {9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5};
+	//System.out.println(Arrays.toString(ary));
+	//Merge.mergesort(ary);
+	// System.out.println("Your sorted array" + "\n" + Arrays.toString(ary));
+
+	// System.out.println();
+	
+	// int[] ary1 = {9,5,8,7,5,6,5,-3,4,5,3,4,5,1,2,3,4,4,3,5,4,3};
+	// System.out.println(Arrays.toString(ary1));
+	// Merge.mergesort(ary1);
+	// System.out.println("Your sorted array" + "\n" + Arrays.toString(ary1));
+
+	// System.out.println();
+			   
+	// int[] ary2 = {3,6,1,2,4,6,8,99,1,2,32,45,123,-23,12,-30,-12,-3};
+	// System.out.println(Arrays.toString(ary2));
+        // Merge.mergesort(ary2);
+	// System.out.println("Your sorted array" + "\n" + Arrays.toString(ary2));
+	// System.out.println("The correct array\n[-30, -23, -12, -3, 1, 1, 2, 2, 3, 4, 6, 6, 8, 12, 32, 45, 99, 123]");
+
+	// System.out.println();
+
+	// int[] ary3 = {-1,3,3,-1,3,3,3,3,3,3,3,3,-1};
+	// System.out.println(Arrays.toString(ary3));
+	// Merge.mergesort(ary3);
+	// System.out.println("Your sorted array" + "\n" + Arrays.toString(ary3));
+	int[] ary4 = {-1,3,4,2,3,3,21,4,5,6,1,9};
+	Merge.mergesort(ary4);
+	System.out.println("Your sorted array" + "\n" + Arrays.toString(ary4));
+	
+    }
+}
