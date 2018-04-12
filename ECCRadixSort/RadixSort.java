f90880f0e3b5d815da11aa0d1f6d00ba95d07300
ivzhang1 <izhang1@stuy.edu>
2018-04-11 23:50:17 -0400

    ?

---
+++ ECCRadixSort/RadixSort.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+public class RadixSort{
+    public static void main(String[] args){
+	int[] x = {1,2,4,3,6,5,3,1,4,7};
+	LinkedList<Integer> z = new LinkedList<>();
+	for (int k: x){
+	    z.add(k);
+	}
+       
+    }
+
+    public void radix(LinkedList<Integer> l){
+	int[] bucket = new int[10];
+    }
+    
+}
