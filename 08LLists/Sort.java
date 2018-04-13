601d0cb74ff48b4bf7918f10176c34de7125b23c
ivzhang1 <izhang1@stuy.edu>
2018-04-12 20:07:26 -0400

    BAD

--- 08LLists/Sort.java
+++ 08LLists/Sort.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Sort{
     public static void main(String[] args){
 	MyLinkedListImproved<Integer> x = new MyLinkedListImproved<>();
@@ -10,9 +12,34 @@ public class Sort{
     
     public static void radixsort(MyLinkedListImproved<Integer> l){
 	int maxN = getNDigs(l.get(l.max()));
-	for(int i = 0; i < 3; i++){
-	    //System.out.println(maxN);
-	    System.out.println(getPos(1000, i));
+	ArrayList<MyLinkedListImproved<Integer>> chumBuk = new ArrayList<>(10);
+	for (int p = 0; p < 10; p++){
+	    chumBuk.add(null);
+	}
+	
+	int dig = 10;
+	for(int i = 0; i < maxN; i++){
+	    for(Integer x: l){
+		if(getNDigs(x) > i){
+		    dig = getPos(x, i);
+		    if(chumBuk.get(dig) == null){
+			MyLinkedListImproved<Integer> k = new MyLinkedListImproved<>();
+			k.add(x);
+			chumBuk.set(dig, k);
+			System.out.print("Go");
+			
+		    }
+		    else{
+			chumBuk.get(dig).add(x);
+		    }
+		}
+	    }
+	    l.clear();
+	    for(MyLinkedListImproved<Integer> y: chumBuk){					
+		l.extend(y);
+	    }
+	    
+	    chumBuk = new ArrayList<>(10);
 	}
     }
 
