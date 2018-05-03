84f2d28aa782883b9a21cbfc6697e4ae96ebed12
ivzhang1 <izhang1@stuy.edu>
2018-05-03 00:53:17 -0400

    commenced heapSort

--- 12HeapsOCash/Sorts.java
+++ 12HeapsOCash/Sorts.java
@@ -9,10 +9,23 @@ public class Sorts{
 	System.out.println(toString(heap));
 	heapify(heap, heap.length-1);
 	System.out.println(toString(heap));
+	heapsort(heap);
+	System.out.println(toString(heap));
 
     }
     
-    public static void heapsort(int[] ary){}
+    public static void heapsort(int[] ary){
+	heapify(ary, ary.length-1);
+	heapsortH(ary, ary.length-1);
+    }
+
+    public static void heapsortH(int[] ary, int end){
+	if(end > 0){
+	    swap(ary, 0, end);
+	    pushDown(ary, ary[0], 0, end);
+	    heapsortH(ary, end-1);
+	}
+    }
     
     public static void heapify(int[] mess, int i){
 	if(i > 0){
@@ -21,18 +34,18 @@ public class Sorts{
 	}
     }
     
-    private static void pushDown(int[] heap, int c, int starting){
+    private static void pushDown(int[] heap, int c, int starting, int end){
 	int id = 2 * starting;
 
-	if(id+1 >= heap.length || id+2 >= heap.length){
+	if(id+1 >= end || id+2 >= end){
 	}
 	else if(heap[id + 1] > c && heap[id + 1] > heap[id+2]){
 	    swap(heap, starting, id + 1);
-	    pushDown(heap, c, id+1);
+	    pushDown(heap, c, id+1, end);
 	}
 	else if(heap[id + 2] > c && heap[id + 2] > heap[id+1]){
 	    swap(heap, starting, id + 2);
-	    pushDown(heap, c, id + 2);
+	    pushDown(heap, c, id + 2, end);
 	}
     }
 
